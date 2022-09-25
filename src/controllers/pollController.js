ecaf7a7a65fce56eea0d2887585f9d54dea90c2a
Kássia <kassia_guterres@outlook.com>
2022-09-25 17:06:05 -0300

    Criação para mostrar os resultados.

--- src/controllers/pollController.js
+++ src/controllers/pollController.js
@@ -1,6 +1,7 @@
 import { ObjectId } from "bson";
 import dayjs from "dayjs";
-import mongo from "../db/db.js"
+import { maxHeaderSize } from "http";
+import mongo from "../db/db.js";
 
 const db = await mongo();
 
@@ -13,24 +14,24 @@ async function createPoll(req, res) {
       const poll = { title, expireAt: defaultDays.format("YYYY-MM-D hh:mm") };
 
       const result = await db.collection("polls").insertOne(poll);
-     
+
       return res.status(201).send("Enquete criada com sucesso!");
     }
-    
-    const expireDate = dayjs().add(expireAt, 'day');
-    await db.collection("polls").insertOne({ title, expireAt, });
+
+    const expireDate = dayjs().add(expireAt, "day");
+    await db.collection("polls").insertOne({ title, expireAt });
     return res.status(201).send("Enquete criada com sucesso");
-  }
-   catch (error) {
+  } catch (error) {
     console.log(error);
     return res.status(500);
   }
 }
 
-async function showPolls( req, res) {
-   const polls = await db.collection("polls").find({}).toArray();
+async function showPolls(req, res) {
+  const polls = await db.collection("polls").find({}).toArray();
   try {
-       if (polls.length === 0) { //lista vazia, nenhuma enquete foi cadastrada 
+    if (polls.length === 0) {
+      //lista vazia, nenhuma enquete foi cadastrada
       return res.status(204).send("Nenhuma enquete cadastrada");
     }
     return res.status(200).send(polls);
@@ -40,19 +41,62 @@ async function showPolls( req, res) {
   }
 }
 
-async function showPollChoices (req, res){
-const pollId = req.params.id;
-//console.log(pollId)
-    try {
-      const choicesByPollId = await db.collection("choices").find({pollId: ObjectId(pollId)}).toArray(); 
-      console.log(choicesByPollId)
-      if(choicesByPollId.length === 0 ){
-        return res.status(404).send("Enquete não encontrada");
-      }
-      return res.send(choicesByPollId);
-    } catch (error) {
-      return res.sendStatus(500);
+async function showPollChoices(req, res) {
+  const pollId = req.params.id;
+
+  try {
+    const choicesByPollId = await db
+      .collection("choices")
+      .find({ pollId: ObjectId(pollId) })
+      .toArray();
+    console.log(choicesByPollId);
+    if (choicesByPollId.length === 0) {
+      return res.status(404).send("Enquete não encontrada");
     }
+    return res.send(choicesByPollId);
+  } catch (error) {
+    return res.sendStatus(500);
+  }
 }
 
-export { createPoll, showPolls, showPollChoices };
+async function showResult(req, res) {
+  const pollId = req.params.id;
+
+  try {
+    const findPoll = await db
+      .collection("polls")
+      .findOne({ _id: ObjectId(pollId) });
+    if (findPoll === null) {
+      return res.sendStatus(404);
+    }
+
+    const findChoices = await db
+      .collection("choices")
+      .find({ pollId: ObjectId(pollId) })
+      .toArray();
+    let choiceMoreVoted = findChoices[0];
+
+    findChoices.forEach((element) => {
+      if (element.votes > choiceMoreVoted.votes) {
+        choiceMoreVoted = element;
+      }
+    });
+
+    const result = {
+      _id: pollId,
+      title: findPoll.title,
+      expireAt: findPoll.expireAt,
+      result: {
+        title: choiceMoreVoted.title,
+        votes: choiceMoreVoted.votes,
+      },
+    };
+    
+    return res.send(result);
+  
+  } catch (error) {
+    return res.sendStatus(500);
+  }
+}
+
+export { createPoll, showPolls, showPollChoices, showResult };
--- src/routes/pollRoute.js
+++ src/routes/pollRoute.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { createPoll, showPolls, showPollChoices } from "../controllers/pollController.js"
+import { createPoll, showPolls, showPollChoices, showResult } from "../controllers/pollController.js"
 import { pollValidation } from "../middlewares/pollMiddlewares.js"
 
 const router = express.Router();
 router.post("/poll", pollValidation, createPoll);
 router.get("/poll", showPolls);
 router.get("/poll/:id/choice", showPollChoices);
+router.get("/poll/:id/result", showResult);
 
 export default router;
